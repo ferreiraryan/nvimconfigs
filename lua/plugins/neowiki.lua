0bca2ab46a95539015e706fbb01cefd577865ffc
ferreiraryan <ryanferreira4883@gmail.com>
2025-09-01 16:04:32 -0300

    Feat: Wiki local

--- lua/plugins/neowiki.lua
+++ lua/plugins/neowiki.lua
@@ -9,13 +9,13 @@ return {
     wiki_dirs = {
       -- neowiki.nvim supports both absolute and tilde-expanded paths
       path = vim.fn.expand '~/GoogleDrive/vimwiki',
-      -- { name = 'Work', path = '~/work/wiki' },
-      -- { name = 'Personal', path = 'personal/wiki' },
+      { name = 'Local Wiki', path = '~/local/local' },
+      { name = 'Personal',   path = 'personal/wiki' },
     },
   },
   keys = {
-    { '<leader>ww', "<cmd>lua require('neowiki').open_wiki()<cr>", desc = 'Open Wiki' },
+    { '<leader>ww', "<cmd>lua require('neowiki').open_wiki()<cr>",          desc = 'Open Wiki' },
     { '<leader>wW', "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = 'Open Wiki in Floating Window' },
-    { '<leader>wT', "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = 'Open Wiki in Tab' },
+    { '<leader>wT', "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>",  desc = 'Open Wiki in Tab' },
   },
 }
